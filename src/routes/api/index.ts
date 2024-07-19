5f80d55c58146b81c86717b082d5c5f4d00362f4
sam sylva <sylvasam24@gmail.com>
2024-07-19 14:07:31 +0100

    first commit

---
+++ src/routes/api/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+const sharp = require('sharp');
+const http = require('http');
+const fs = require('fs');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+  const newImage = (async function () {
+    try {
+      const name = req.query.name as unknown as string;
+      const height = req.query.height as unknown as string;
+      const width = req.query.width as unknown as string;
+
+      await sharp('src/assets/full/fjord.jpg')
+        .resize(parseInt(height), parseInt(width))
+        .toFile(`src/assets/thumbs/${name}.png`);
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+  fs.readFile(
+    'src\\assets\\thumbs\\image.png',
+    function (err: unknown, data: unknown) {
+      res.writeHead(200, { 'Content-Type': 'image/png' });
+      res.write(data);
+      return res.end();
+    },
+  );
+});
+
+export default routes;
